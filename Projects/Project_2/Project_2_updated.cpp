d39a395f520e3d783fc4f6b885148309894df3b2
Shou <zaine00@gmail.com>
2019-03-05 11:09:33 -0500

    B B B

---
+++ Projects/Project_2/Project_2_updated.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "This program will display all prime numbers between 1-20." << endl;
+    cout << "Please enter a number between 1-20: ";
+    cin >> num;
+    while (num<1||num>20)
+    {
+        cout << "That is not a number between 1-20." << endl;
+        cout << "Please enter a number between 1-20: ";
+        cin >> num;
+    }
+    cout << "The prime numbers are: " << endl;
+    bool prime = true;
+    for (int i=3; i <= num; i++)
+    {
+      for(int j=2; j<=(i-1);j++)
+      {
+        if(i%j ==0)
+        {
+          prime =false;
+          break;
+        }
+        else
+        {
+          prime = true;
+        }
+      }
+      if (prime == true)
+      {
+        cout << i << " ";
+      }
+    }
+    return 0;
+}
